6b99f16a32aadcaaeb934f56db3fdb9b77f1e60b
Sehoon Jeong <wtd120120@gmail.com>
2023-06-09 14:05:23 +0900

    [Chore] #15 코드 컨벤션 및 기타 수정
    - 명칭 수정: AddressBubble -> AddressBubbleView 로 변경
    - 기존 Default 값 -> DefaultText enum추가
    - indentation 수정

--- Projects/Cafe/Sources/View/MarkerView.swift
+++ Projects/Cafe/Sources/View/MarkerView.swift
@@ -7,74 +7,76 @@
 import SwiftUI
 
 struct MarkerView: View {
-  var cafeName: String = "학림 다방"
-  var subTitle: String = "서울특별시 종로구 대학로 119"
-  var description: String = ""
+  enum DefaultText {
+    static let cafeName = "학림 다방"
+    static let subTitle = "서울특별시 종로구 대학로 119"
+    static let description = ""
+  }
 
-    var body: some View {
-      VStack {
-        VStack(alignment: .leading) {
-          Text(cafeName)
-            .font(.headline)
-          Text(subTitle)
-            .font(.subheadline)
-            .foregroundColor(.gray)
-          Divider()
-          Text(description)
-          Text("평점: 5.0, 공부하기 좋은 카페")
-        }
-        .padding(.top, 10)
-        .padding(.bottom, 17)
-        .padding(.horizontal, 17)
-        .background(AddressBubble())
-        .padding(.bottom, 3)
-
-        Image("coffee")
-          .resizable()
-          .scaledToFit()
-          .frame(width: 20)
+  var body: some View {
+    VStack {
+      VStack(alignment: .leading) {
+        Text(DefaultText.cafeName)
+          .font(.headline)
+        Text(DefaultText.subTitle)
+          .font(.subheadline)
+          .foregroundColor(.gray)
+        Divider()
+        Text(DefaultText.description)
+        Text("평점: 5.0, 공부하기 좋은 카페")
       }
+      .padding(.top, 10)
+      .padding(.bottom, 17)
+      .padding(.horizontal, 17)
+      .background(AddressBubbleView())
+      .padding(.bottom, 3)
+
+      Image("coffee")
+        .resizable()
+        .scaledToFit()
+        .frame(width: 20)
     }
+  }
 }
 
-struct AddressBubble: View {
-    var body: some View {
-        ZStack(alignment: .bottom) {
-            VStack(spacing: 0) {
-                RoundedRectangle(cornerRadius: 12)
-                    .stroke(lineWidth: 1)
-                    .background(Color.white)
-                    .cornerRadius(12)
+struct AddressBubbleView: View {
+  var body: some View {
+    ZStack(alignment: .bottom) {
+      VStack(spacing: 0) {
+        RoundedRectangle(cornerRadius: 12)
+          .stroke(lineWidth: 1)
+          .background(Color.white)
+          .cornerRadius(12)
 
-                Spacer()
-            }
-            Triangle()
-                .stroke(lineWidth: 1)
-                .frame(width: 12, height: 8)
-                .background(Color.white)
-                .clipShape(Triangle())
+        Spacer()
+      }
+      Triangle()
+        .stroke(lineWidth: 1)
+        .frame(width: 12, height: 8)
+        .background(Color.white)
+        .clipShape(Triangle())
 
-            Rectangle()
-                .frame(width: 10.5, height: 2)
-                .foregroundColor(.white)
-                .padding(.bottom, 7.5)
-        }
+      Rectangle()
+        .frame(width: 10.5, height: 2)
+        .foregroundColor(.white)
+        .padding(.bottom, 7.5)
     }
+  }
 
-    struct Triangle: Shape {
-        func path(in rect: CGRect) -> Path {
-            var path = Path()
-            path.move(to: CGPoint(x: rect.midX, y: rect.maxY))
-            path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
-            path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
-            path.addLine(to: CGPoint(x: rect.midX, y: rect.maxY))
-            return path
-        }
+  struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+      var path = Path()
+      path.move(to: CGPoint(x: rect.midX, y: rect.maxY))
+      path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
+      path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+      path.addLine(to: CGPoint(x: rect.midX, y: rect.maxY))
+      return path
     }
+  }
 }
 
 struct MarkerView_Previews: PreviewProvider {
-    static var previews: some View {
-        MarkerView()
-    }
+  static var previews: some View {
+    MarkerView()
+  }
 }
