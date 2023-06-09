2465cdc3902d93827650744879ace0d2c62710eb
Sehoon Jeong <wtd120120@gmail.com>
2023-06-09 13:56:59 +0900

    [Chore] #15 코드 컨벤션 수정
    - 메서드 사이 line spacing 추가
    - Delegate 메서드 구분
    - 변수명 수정 ( authorization -> authorizationStatus)
    -  var -> let 수정: static let liveValue

--- Projects/Cafe/Sources/LocationManager.swift
+++ Projects/Cafe/Sources/LocationManager.swift
@@ -11,26 +11,29 @@ import CoreLocation
 import Dependencies
 import ComposableArchitecture
 
-class LocationManager: NSObject, DependencyKey, CLLocationManagerDelegate {
-  static var liveValue: LocationManager = .init()
+final class LocationManager: NSObject, DependencyKey {
+  static let liveValue: LocationManager = .init()
   private let manager = CLLocationManager()
   private var region = CLLocationCoordinate2D()
-  private var authorization: CLAuthorizationStatus
+  private var authorizationStatus: CLAuthorizationStatus
   private var isLocationReturend: CheckedContinuation<CLLocationCoordinate2D, Error>?
   private var isAuthorizationReturned: CheckedContinuation<CLAuthorizationStatus, Never>?
+
   override init() {
-    authorization = manager.authorizationStatus
+    authorizationStatus = manager.authorizationStatus
     super.init()
     manager.delegate = self
     manager.desiredAccuracy = kCLLocationAccuracyBest
   }
+
   func requestAuthorization() {
     manager.requestWhenInUseAuthorization()
   }
+
   func fetchCurrentLocation() async throws -> CLLocationCoordinate2D {
       return try await withCheckedThrowingContinuation { [weak self] continuation in
         self?.isLocationReturend = continuation
-        if self?.authorization == .authorizedWhenInUse || self?.authorization == .authorizedAlways {
+        if self?.authorizationStatus == .authorizedWhenInUse || self?.authorizationStatus == .authorizedAlways {
           self?.manager.requestLocation()
         } else {
           self?.isLocationReturend?.resume(returning: CLLocationCoordinate2D(latitude: 37.5819, longitude: 127.0014))
@@ -39,27 +42,31 @@ class LocationManager: NSObject, DependencyKey, CLLocationManagerDelegate {
       }
   }
 }
+
 extension DependencyValues {
   var locationManager: LocationManager {
     get { self[LocationManager.self] }
     set { self[LocationManager.self] = newValue }
   }
 }
-extension LocationManager {
+
+extension LocationManager: CLLocationManagerDelegate {
   // TODO: 위치권한 거부 및 앱 설정 변경 필요한 케이스 분기 처리
   func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
-    authorization = manager.authorizationStatus
+    authorizationStatus = manager.authorizationStatus
     // TODO: 위치 권한 설정 변경 시, Async처리 할 수 있도록 수정
     if manager.authorizationStatus == .authorizedAlways || manager.authorizationStatus == .authorizedWhenInUse {
       isAuthorizationReturned?.resume(returning: manager.authorizationStatus)
       isAuthorizationReturned = nil
     }
   }
+
   func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
     region = locations.first?.coordinate ??  CLLocationCoordinate2D(latitude: 37.5819, longitude: 127.0017)
     isLocationReturend?.resume(returning: region)
     isLocationReturend = nil
   }
+
   func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
     isLocationReturend?.resume(throwing: error)
     isLocationReturend = nil
