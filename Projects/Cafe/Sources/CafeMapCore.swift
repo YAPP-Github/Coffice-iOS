29e24b4a0b80ca35a8be567d0eb08600fd675ac3
Sehoon Jeong <wtd120120@gmail.com>
2023-06-11 00:24:39 +0900

    [Fix] #15 CLLocationCoordinate2D Equatable 프로토콜 수정
    - 조건이 올바르게 정의되지 않아 현재위치 조회 시, 기존 region 상태 값이 변화가 없음에도 mapView Update 발생

--- Projects/Cafe/Sources/CafeMapCore.swift
+++ Projects/Cafe/Sources/CafeMapCore.swift
@@ -12,7 +12,7 @@ import CoreLocation
 
 extension CLLocationCoordinate2D: Equatable {
   public static func == (lhs: CLLocationCoordinate2D, rhs: CLLocationCoordinate2D) -> Bool {
-    lhs.latitude == rhs.longitude
+    return lhs.latitude == rhs.latitude && lhs.longitude == rhs.longitude
   }
 }
 
