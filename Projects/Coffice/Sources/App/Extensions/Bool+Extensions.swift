d28babcc7c887a96bf60802da31937cee38f529b
applebuddy <eagermin92@gmail.com>
2023-06-19 02:30:23 +0900

    [Feat] #38 isEmpty, isNot 관련 계산 프로퍼티 extension 추가
    !foo.isEmpty, !flag 대신, foo.isNotEmpty, flag.isNot 과 같은 방식으로 이해하기 쉽게 코드를 작성할 수 있도록 extension 추가

---
+++ Projects/Coffice/Sources/App/Extensions/Bool+Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  Bool+Extensions.swift
+//  coffice
+//
+//  Created by Min Min on 2023/06/18.
+//  Copyright © 2023 kr.co.yapp. All rights reserved.
+//
+
+import Foundation
+
+extension Bool {
+  var isNot: Bool { !self }
+}
---
+++ Projects/Coffice/Sources/App/Extensions/String+Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  String+Extensions.swift
+//  coffice
+//
+//  Created by Min Min on 2023/06/18.
+//  Copyright © 2023 kr.co.yapp. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+  var isNotEmpty: Bool { !isEmpty }
+}
