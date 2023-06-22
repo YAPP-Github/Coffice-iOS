9b7859b88f38eb14fb736fa42ac4afcfd9754abc
Neph <neph3779@gmail.com>
2023-06-23 00:10:28 +0900

    [Feat] #40 검색결과DTO, APIClient 제작 (#43)

---
+++ Projects/Network/Sources/DTOs/SearchWordResponseDTO.swift
@@ -0,0 +1,15 @@
+//
+//  SearchWordResponseDTO.swift
+//  Network
+//
+//  Created by 천수현 on 2023/06/22.
+//  Copyright © 2023 kr.co.yapp. All rights reserved.
+//
+
+import Foundation
+
+public struct SearchWordResponseDTO: Decodable {
+  public let searchWordId: Int
+  public let text: String
+  public let createdAt: String
+}
