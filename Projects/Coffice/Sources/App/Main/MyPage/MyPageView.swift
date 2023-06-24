aa42f900eb4e4dcb92daa4ee2a6d20f8f904a8da
Neph <neph3779@gmail.com>
2023-06-24 19:17:06 +0900

    [Refactor] #44 viewStore 사용방식 WithViewStore로 변경

--- Projects/Coffice/Sources/App/Main/MyPage/MyPageView.swift
+++ Projects/Coffice/Sources/App/Main/MyPage/MyPageView.swift
@@ -11,45 +11,63 @@ import SwiftUI
 
 struct MyPageView: View {
   private let store: StoreOf<MyPage>
-  @ObservedObject private var viewStore: ViewStoreOf<MyPage>
 
   init(store: StoreOf<MyPage>) {
     self.store = store
-    viewStore = ViewStore(store, observe: { $0 })
   }
 
   var body: some View {
-    mainView
-      .onAppear {
-        viewStore.send(.onAppear)
-      }
+    WithViewStore(store) { viewStore in
+      mainView
+        .onAppear {
+          viewStore.send(.onAppear)
+        }
+    }
   }
 
   private var mainView: some View {
-    VStack(alignment: .leading, spacing: 0) {
-      nickNameView
-      divider
-      linkedAccountTypeView
-      divider
-      VStack(spacing: 0) {
-        ForEach(viewStore.menuItems) { menuItem in
-          menuItemView(menuItem: menuItem)
-        }
-      }
-      .padding(.vertical, 10)
+    WithViewStore(store) { viewStore in
 
-      Spacer()
+      VStack(alignment: .leading, spacing: 0) {
+        nickNameView
+        divider
+        linkedAccountTypeView
+        divider
+        VStack(spacing: 0) {
+          ForEach(viewStore.menuItems) { menuItem in
+            menuItemView(menuItem: menuItem)
+          }
+        }
+        .padding(.vertical, 10)
+
+        Spacer()
+      }
+      .padding(.horizontal, 16)
+      .padding(.top, 50)
     }
-    .padding(.horizontal, 16)
-    .padding(.top, 50)
   }
 
   private var nickNameView: some View {
-    Group {
-      if viewStore.state.loginType == .anonymous {
-        Button {
-          viewStore.send(.presentLoginPage)
-        } label: {
+    WithViewStore(store) { viewStore in
+
+      Group {
+        if viewStore.state.loginType == .anonymous {
+          Button {
+            viewStore.send(.presentLoginPage)
+          } label: {
+            HStack(alignment: .center, spacing: 0) {
+              Text(viewStore.state.nickName)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .font(.title)
+              Button {
+                viewStore.send(.presentLoginPage)
+              } label: {
+                Image(systemName: "chevron.right")
+                  .imageScale(.large)
+              }
+            }
+          }
+        } else {
           HStack(alignment: .center, spacing: 0) {
             Text(viewStore.state.nickName)
               .frame(maxWidth: .infinity, alignment: .leading)
@@ -60,50 +78,44 @@ struct MyPageView: View {
               Image(systemName: "chevron.right")
                 .imageScale(.large)
             }
+            .tint(.black)
           }
-        }
-      } else {
-        HStack(alignment: .center, spacing: 0) {
-          Text(viewStore.state.nickName)
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .font(.title)
-          Button {
-            viewStore.send(.presentLoginPage)
-          } label: {
-            Image(systemName: "chevron.right")
-              .imageScale(.large)
-          }
-          .tint(.black)
         }
       }
+      .padding(.vertical, 30)
+      .tint(.black)
     }
-    .padding(.vertical, 30)
-    .tint(.black)
   }
 
   private var linkedAccountTypeView: some View {
-    HStack(alignment: .center, spacing: 0) {
-      Text("연결된 계정")
-        .frame(maxWidth: .infinity, alignment: .leading)
-      Text(viewStore.loginType.displayName)
-        .padding(.trailing, 10)
+    WithViewStore(store) { viewStore in
+
+      HStack(alignment: .center, spacing: 0) {
+        Text("연결된 계정")
+          .frame(maxWidth: .infinity, alignment: .leading)
+        Text(viewStore.loginType.displayName)
+          .padding(.trailing, 10)
+      }
+      .padding(.vertical, 30)
     }
-    .padding(.vertical, 30)
   }
 
   private func menuItemView(menuItem: MyPage.MenuItem) -> some View {
-    VStack(alignment: .leading, spacing: 0) {
-      HStack {
-        Button {
-          viewStore.send(.menuClicked(menuItem))
-        } label: {
-          Text(menuItem.title)
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .frame(height: 50)
-          Image(systemName: "chevron.right")
+    WithViewStore(store) { viewStore in
+
+      VStack(alignment: .leading, spacing: 0) {
+        HStack {
+          Button {
+            viewStore.send(.menuClicked(menuItem))
+          } label: {
+            Text(menuItem.title)
+              .frame(maxWidth: .infinity, alignment: .leading)
+              .frame(height: 50)
+            Image(systemName: "chevron.right")
+          }
         }
+        .tint(.black)
       }
-      .tint(.black)
     }
   }
 
