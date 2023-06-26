c6af88a8c25506100ac931f9b8355292d685e9dd
Applebuddy <eagermin92@gmail.com>
2023-06-26 22:36:03 +0900

    Feature/#66 update tabbar UI (#67)
    * [UI] #66 탭바 아이콘 Image Asset 추가
    
    * [UI] #66 탭바 UI 수정
    
    - 현 figma를 참고해서 탭바 UI를 수정
    - 사용되지 않는 첫번째 탭바 메뉴를 제거 후, 지도화면이 메인으로 오도록 수정
    
    ---------
    
    Co-authored-by: ios-min <kt.min@bucketplace.net>

--- Projects/Coffice/Sources/App/TabBarCore.swift
+++ Projects/Coffice/Sources/App/TabBarCore.swift
@@ -13,26 +13,25 @@ import TCACoordinators
 struct TabBar: ReducerProtocol {
   struct State: Equatable {
     static let initialState: State = .init()
-    private let tabBarItemTypes: [TabBarItemType] = [.home, .search, .savedList, .myPage]
+    private let tabBarItemTypes: [TabBarItemType] = [.search, .savedList, .myPage]
     private(set) var isTabBarViewPresented = true
-    private(set) var tabBarItemViewModels: [TabBarItemViewModel] = [
-      .init(type: .home, isSelected: true),
-      .init(type: .search, isSelected: false),
+    private(set) var tabBarItemViewStates: [TabBarItemViewState] = [
+      .init(type: .search, isSelected: true),
       .init(type: .savedList, isSelected: false),
       .init(type: .myPage, isSelected: false)
     ]
 
-    var selectedTab: TabBarItemType = .home {
+    var selectedTab: TabBarItemType = .search {
       didSet {
-        tabBarItemViewModels = tabBarItemTypes.map {
-          TabBarItemViewModel(type: $0, isSelected: selectedTab == $0)
+        tabBarItemViewStates = tabBarItemTypes.map {
+          TabBarItemViewState(type: $0, isSelected: selectedTab == $0)
         }
       }
     }
   }
 
   enum Action: Equatable {
-    case selectTab(TabBarItemType)
+    case selectTab(TabBar.State.TabBarItemType)
     case tabBarView(isPresented: Bool)
   }
 
@@ -45,6 +44,60 @@ struct TabBar: ReducerProtocol {
 
       default:
         return .none
+      }
+    }
+  }
+}
+
+extension TabBar.State {
+  enum TabBarItemType: CaseIterable {
+    case search
+    case savedList
+    case myPage
+  }
+
+  struct TabBarItemViewState: Hashable {
+    let type: TabBarItemType
+    let isSelected: Bool
+
+    init(type: TabBarItemType, isSelected: Bool = false) {
+      self.type = type
+      self.isSelected = isSelected
+    }
+
+    var title: String {
+      switch type {
+      case .search:
+        return "홈"
+      case .savedList:
+        return "저장"
+      case .myPage:
+        return "MY"
+      }
+    }
+
+    var image: CofficeImages {
+      switch type {
+      case .search:
+        if isSelected {
+          return CofficeAsset.Asset.homeNaviSelected44px
+        } else {
+          return CofficeAsset.Asset.homeNaviUnselected44px
+        }
+
+      case .savedList:
+        if isSelected {
+          return CofficeAsset.Asset.bookmarkNaviSelected44px
+        } else {
+          return CofficeAsset.Asset.bookmarkNaviUnselected44px
+        }
+
+      case .myPage:
+        if isSelected {
+          return CofficeAsset.Asset.mypageNaviSelected44px
+        } else {
+          return CofficeAsset.Asset.mypageNaviUnselected44px
+        }
       }
     }
   }
