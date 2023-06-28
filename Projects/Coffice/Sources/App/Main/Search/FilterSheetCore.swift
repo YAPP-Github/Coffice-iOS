f7c723aa2284f76ba272498a080b21dc5a086716
Sehoon Jeong <wtd120120@gmail.com>
2023-06-28 15:23:04 +0900

    [Chore] #64 네이밍 및 기타수정

--- Projects/Coffice/Sources/App/Main/Search/FilterSheetCore.swift
+++ Projects/Coffice/Sources/App/Main/Search/FilterSheetCore.swift
@@ -11,8 +11,7 @@ import ComposableArchitecture
 
 struct FilterSheetCore: ReducerProtocol {
   struct State: Equatable {
-    var none: [Int] = []
-    var filterType: FilterType = .outletFilter
+    var filterType: FilterType = .none
     var cafeSizeFilterButtons  = CafeSizeFilterButton.allCases
     var cafeOutletFilterButtons = CafeOutletFilterButton.allCases
     var cafeOperationHourFilterButtons = CafeOperationHourFilterButton.allCases
@@ -20,6 +19,7 @@ struct FilterSheetCore: ReducerProtocol {
   }
 
   enum Action: Equatable {
+    case dismiss
     case filterCategoryButtonTapped(FilterType)
     case cafeOutletFilterButtonTapped(CafeOutletFilterButton)
     case cafeSizeFilterButtonTapped(CafeSizeFilterButton)
@@ -31,20 +31,20 @@ struct FilterSheetCore: ReducerProtocol {
       switch action {
       case .filterCategoryButtonTapped(let filterType):
         switch filterType {
-        case .cafeOperationHourFilter:
-          state.filterType = .cafeOperationHourFilter
+        case .runningTime:
+          state.filterType = .runningTime
           return .none
 
-        case .outletFilter:
-          state.filterType = .outletFilter
+        case .outlet:
+          state.filterType = .outlet
           return .none
 
-        case .cafeSizeFilter:
-          state.filterType = .cafeSizeFilter
+        case .spaceSize:
+          state.filterType = .spaceSize
           return .none
 
-        case .cafePersonnelFilter:
-          state.filterType = .cafePersonnelFilter
+        case .personnel:
+          state.filterType = .personnel
           return .none
 
         case .cafeDetailFilter:
@@ -74,23 +74,28 @@ extension FilterSheetCore {
 
     var name: String {
       switch self {
-      case .enough:
-        return "넉넉(80%)"
-      case .some:
-        return "보통(50%)"
-      case .little:
-        return "부족(10%)"
-      case .none:
-        return ""
+      case .enough: return "넉넉(80%)"
+      case .some: return "보통(50%)"
+      case .little: return "부족(10%)"
+      case .none: return ""
       }
     }
   }
 
   enum CafeSizeFilterButton: CaseIterable {
     case large
-    case middle
+    case medium
     case small
     case none
+
+    var name: String {
+      switch self {
+      case .large: return "대형"
+      case .medium: return "중형"
+      case .small: return "소형"
+      case .none: return ""
+      }
+    }
   }
 
   enum CafeOperationHourFilterButton: CaseIterable {
@@ -98,34 +103,45 @@ extension FilterSheetCore {
     case onTime
     case twentyFourHours
     case none
+
+    var name: String {
+      switch self {
+      case .clock: return "넉넉(80%)"
+      case .onTime: return "영업중"
+      case .twentyFourHours: return "24시간"
+      case .none: return ""
+      }
+    }
   }
 
   enum CafePersonnelFilterButton: CaseIterable {
     case moreFivePeople
     case none
+
+    var name: String {
+      switch self {
+      case .moreFivePeople: return "5인이상"
+      case .none: return ""
+      }
+    }
   }
 
-  enum FilterType {
-    case outletFilter
-    case cafeOperationHourFilter
-    case cafeSizeFilter
+  enum FilterType: CaseIterable {
+    case outlet
+    case runningTime
+    case spaceSize
     case cafeDetailFilter
-    case cafePersonnelFilter
+    case personnel
     case none
+
     var titleName: String {
       switch self {
-      case .outletFilter:
-        return "콘센트"
-      case .cafeOperationHourFilter:
-        return "영업시간"
-      case .cafePersonnelFilter:
-        return "인원"
-      case .cafeSizeFilter:
-        return "공간크기"
-      case .cafeDetailFilter:
-        return "세부필터"
-      case .none:
-        return ""
+      case .outlet: return "콘센트"
+      case .runningTime: return "영업시간"
+      case .personnel: return "인원"
+      case .spaceSize: return "공간크기"
+      case .cafeDetailFilter: return "세부필터"
+      case .none: return ""
       }
     }
   }
