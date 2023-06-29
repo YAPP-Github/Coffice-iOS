73e936b209adb01682a7aa64ea89796233c56fb5
Sehoon Jeong <wtd120120@gmail.com>
2023-06-29 16:12:11 +0900

    [Feat] #70 TabBarPreferenceKey 구현

---
+++ Projects/Coffice/Sources/App/PreferenceKeys/TabBarPreferenceKey.swift
@@ -0,0 +1,17 @@
+//
+//  TabbarPreferenceKey.swift
+//  coffice
+//
+//  Created by sehooon on 2023/06/29.
+//  Copyright © 2023 kr.co.yapp. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+struct TabBarPreferenceKey: PreferenceKey {
+  static var defaultValue: CGSize = .zero
+  static func reduce(value: inout CGSize, nextValue: () -> CGSize ) {
+    value = nextValue()
+  }
+}
