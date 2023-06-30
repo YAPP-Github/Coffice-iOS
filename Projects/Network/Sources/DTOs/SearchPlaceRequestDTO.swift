bb3f936ee095e4ddfdf38d03db53a0fbeacd3378
Neph <neph3779@gmail.com>
2023-06-30 17:28:19 +0900

    [Feat] #79 매장 검색 api 연동

--- Projects/Network/Sources/DTOs/SearchPlaceRequestDTO.swift
+++ Projects/Network/Sources/DTOs/SearchPlaceRequestDTO.swift
@@ -9,18 +9,28 @@
 import Foundation
 
 public struct SearchPlaceRequestDTO: Encodable {
-  public let searchText: String
+  public let searchText: String?
   public let latitude: Double
   public let longitude: Double
   public let distance: Double
+  public let open: Bool?
+  public let hasCommunalTable: Bool?
+  public let capcityLevel: [String]?
+  public let drinkType: [String]?
+  public let foodType: [String]?
   public let pageSize: Int
   public let pageNumber: Int
 
-  public init(searchText: String, latitude: Double, longitude: Double, distance: Double, pageSize: Int, pageNumber: Int) {
+  public init(searchText: String?, latitude: Double, longitude: Double, distance: Double, open: Bool?, hasCommunalTable: Bool?, capcityLevel: [String]?, drinkType: [String]?, foodType: [String]?, pageSize: Int, pageNumber: Int) {
     self.searchText = searchText
     self.latitude = latitude
     self.longitude = longitude
     self.distance = distance
+    self.open = open
+    self.hasCommunalTable = hasCommunalTable
+    self.capcityLevel = capcityLevel
+    self.drinkType = drinkType
+    self.foodType = foodType
     self.pageSize = pageSize
     self.pageNumber = pageNumber
   }
