888b6ae51a5e27617f4f75e55fdcba9c232f4b5b
Neph <neph3779@gmail.com>
2023-07-01 15:19:34 +0900

    Feature/#74 toast view 제작 (#82)
    * [Chore] #74 TCA 최신버전 import하도록 변경
    
    * [Feat] #74 ToastView 구현
    
    * [Chore] #74 사용하지 않게된 Toast 관련 코드 삭제
    
    * [Chore] #74 CofficeFont Equatable 채택
    
    * [Feat, UI] #74 북마크 버튼 탭시 ToastView 노출 기능 구현
    
    * [Chore] #74 ToastCore 네이밍 변경
    
    * [Chore] #74 store, body 사이 줄바꿈 추가

---
+++ Projects/Coffice/Sources/App/Main/ToastView.swift
@@ -0,0 +1,51 @@
+//
+//  ToastView.swift
+//  coffice
+//
+//  Created by 천수현 on 2023/07/01.
+//  Copyright © 2023 kr.co.yapp. All rights reserved.
+//
+
+import ComposableArchitecture
+import SwiftUI
+
+struct ToastView: View {
+  private let store: StoreOf<Toast>
+
+  init(store: StoreOf<Toast>) {
+    self.store = store
+  }
+
+  var body: some View {
+    WithViewStore(store) { viewStore in
+      VStack {
+        Spacer()
+        HStack {
+          viewStore.state.image.swiftUIImage
+            .renderingMode(.template)
+            .foregroundColor(CofficeAsset.Colors.grayScale1.swiftUIColor)
+          Text(viewStore.state.title)
+            .multilineTextAlignment(.center)
+            .foregroundColor(viewStore.state.config.textColor)
+            .applyCofficeFont(font: viewStore.state.config.font)
+        }
+        .padding(.horizontal, 16)
+        .padding(.vertical, 13)
+        .background(viewStore.state.config.backgroundColor)
+        .cornerRadius(8)
+      }
+      .padding(.bottom, 100)
+    }
+  }
+}
+
+struct ToastView_Previews: PreviewProvider {
+  static var previews: some View {
+    ToastView(
+      store: .init(
+        initialState: .mock,
+        reducer: Toast()
+      )
+    )
+  }
+}
