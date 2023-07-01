752c16b10ae85745da2a8b5ffe56f11d8192da7e
Neph <neph3779@gmail.com>
2023-07-01 14:54:49 +0900

    [Chore] #74 사용하지 않게된 Toast 관련 코드 삭제

--- Projects/Coffice/Sources/App/Extensions/View+Extensions.swift
+++ Projects/Coffice/Sources/App/Extensions/View+Extensions.swift
@@ -56,14 +56,4 @@ extension View {
       )
     )
   }
-
-  func toast(message: String, image: Image, isPresented: Binding<Bool>) -> some View {
-    self.modifier(
-      Toast(
-        message: message,
-        isPresented: isPresented,
-        image: image
-      )
-    )
-  }
 }
