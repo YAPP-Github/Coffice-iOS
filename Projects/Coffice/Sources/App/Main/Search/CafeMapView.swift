c69228aa95e5409ef6f57c4d9ba86e08a59be09d
Sehoon Jeong <wtd120120@gmail.com>
2023-07-02 16:50:51 +0900

    [UI] #77 CardView viewType 설정

--- Projects/Coffice/Sources/App/Main/Search/CafeMapView.swift
+++ Projects/Coffice/Sources/App/Main/Search/CafeMapView.swift
@@ -24,7 +24,7 @@ struct CafeMapView: View {
             floatingButtonView
             .padding()
             Spacer()
-            CafeCardView()
+            CafeCardView(viewType: .cardView)
               .frame(width: geometry.size.width, height: 260)
               .padding(.bottom, TabBarSizePreferenceKey.defaultValue.height)
           }
