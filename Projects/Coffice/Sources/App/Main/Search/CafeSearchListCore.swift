747f47da5f2184c457fa240c8eee195d84585df7
Sehoon Jeong <wtd120120@gmail.com>
2023-07-02 21:23:22 +0900

    [Feat] #77 무한스크롤 기능 테스트 [수정 예정]
    - 무한스크롤 추후 수정

--- Projects/Coffice/Sources/App/Main/Search/CafeSearchListCore.swift
+++ Projects/Coffice/Sources/App/Main/Search/CafeSearchListCore.swift
@@ -7,28 +7,116 @@
 //
 
 import ComposableArchitecture
+import Network
 
 struct CafeSearchListCore: ReducerProtocol {
   struct State: Equatable {
-    let filterOrders = FilterSheetCore.FilterType.allCases
+    // TODO: filterButtonState 구현
+    let filterOrders = FilterType.allCases
     var filterSheetState = FilterSheetCore.State(filterType: .cafeDetailFilter)
+    var data: [SearchPlaceResponseDTO] = []
+    var pageSize: Int = 10
+    var pageNumber: Int = -1
+    var pagenationRange: Range<Int> {
+      pageNumber * pageSize..<(pageNumber + 1) * pageSize - 1
+    }
   }
 
   enum Action: Equatable {
+    case updateState(FilterSheetCore.State)
     case presentFilterSheetView(FilterSheetCore.State)
-    case filterButtonTapped(FilterSheetCore.FilterType)
+    case fetchData(SearchPlaceRequestValue)
+    case dataResponse(TaskResult<[SearchPlaceResponseDTO]>)
+    case filterButtonTapped(FilterType)
+    case scrollAndloadData(Int)
     case dismiss
     case popView
   }
 
+  @Dependency(\.placeAPIClient) private var placeAPIClient
+
   var body: some ReducerProtocolOf<CafeSearchListCore> {
     Reduce { state, action in
       switch action {
+      case .updateState(let bottomSheetState):
+        state.filterSheetState = bottomSheetState
+        return .none
+
+      // TODO: 무한스크롤 추후 수정 예정
+      case .scrollAndloadData(let itemIdx):
+        if state.pagenationRange ~= itemIdx || itemIdx/state.pageSize <= state.pageNumber {
+          print("실패: \(itemIdx)")
+          return .none
+        } else {
+          state.pageNumber += 1
+          return .send(
+            .fetchData(
+              SearchPlaceRequestValue(
+                searchText: "스타", latitude: 37.4982763, longitude: 127.0268507, distacne: 13000000000,
+                isOpened: nil, hasCommunalTable: nil, capcityLevel: nil, drinkType: nil, foodType: nil,
+                pageSize: state.pageSize, pageNumber: state.pageNumber)
+            )
+          )
+        }
+
+      case .fetchData(let requestValue):
+        return .run { send in
+          let result = await TaskResult {
+            let data = try await placeAPIClient.searchPlaces(requestValue: requestValue)
+            return data
+          }
+          await send(.dataResponse(result))
+        }
+
+      case .dataResponse(.success(let data)):
+        state.data += data
+        return .none
+
       case .filterButtonTapped(let filterButton):
-        state.filterSheetState.filterType = filterButton
-        return EffectTask(value: .presentFilterSheetView(state.filterSheetState))
+        switch filterButton {
+        case .outlet:
+          state.filterSheetState.filterType = .outlet
+          return EffectTask(value: .presentFilterSheetView(state.filterSheetState))
+
+        case .spaceSize:
+          return .none
+
+        default:
+          return .none
+        }
+
       default:
         return .none
+      }
+    }
+  }
+}
+
+extension CafeSearchListCore {
+  enum FilterType: CaseIterable {
+    case detail
+    case runningTime
+    case outlet
+    case spaceSize
+    case personnel
+
+    var title: String {
+      switch self {
+      case .detail: return "CofficeAsset.Asset.filterLine24px"
+      case .runningTime: return "영업시간"
+      case .outlet: return "콘센트"
+      case .spaceSize: return "공간크기"
+      case .personnel: return "단체석"
+      }
+    }
+
+    var size: (width: Double, height: Double) {
+      switch self {
+      case .detail: return (width: 56, height: 36)
+      case .runningTime: return (width: 91, height: 36)
+      case .outlet: return (width: 79, height: 36)
+      case .spaceSize: return (width: 91, height: 36)
+      case .personnel: return (width: 69, height: 36)
       }
     }
   }
