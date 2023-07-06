59e64afdafbffb7c9dad73a5984cac063d7bcbbd
Neph <neph3779@gmail.com>
2023-07-06 15:40:46 +0900

    [Refactor] #102 불필요해진 BookmarkCafe를 Cafe로 교체

--- Projects/Coffice/Sources/App/Main/SavedList/SavedListCore.swift
+++ Projects/Coffice/Sources/App/Main/SavedList/SavedListCore.swift
@@ -9,16 +9,12 @@
 import ComposableArchitecture
 
 struct SavedList: ReducerProtocol {
-  struct BookmarkCafe: Hashable {
-    let cafeData: Cafe
-    var isBookmarked: Bool
-  }
 
   struct State: Equatable {
     let title = "저장 리스트"
-    var cafes: [BookmarkCafe] = []
+    var cafes: [Cafe] = []
     var unBookmarkedCafeIds: [Int] {
-      return cafes.filter { $0.isBookmarked.isFalse }.map { $0.cafeData.placeId }
+      return cafes.filter { $0.isBookmarked?.isFalse ?? false }.map { $0.placeId }
     }
     var didFetchComplete: Bool = false
     var shouldShowEmptyListReplaceView: Bool {
@@ -29,8 +25,8 @@ struct SavedList: ReducerProtocol {
   enum Action: Equatable {
     case onAppear
     case onDisappear
-    case bookmarkButtonTapped(cafe: BookmarkCafe)
-    case bookmarkedCafeResponse(cafes: [BookmarkCafe])
+    case bookmarkButtonTapped(cafe: Cafe)
+    case bookmarkedCafeResponse(cafes: [Cafe])
     case deleteCafesFromBookmark
     case pushCafeDetail(cafeId: Int)
   }
@@ -44,7 +40,7 @@ struct SavedList: ReducerProtocol {
         return .run { send in
           let bookmarkedCafes = try await bookmarkClient.fetchMyPlaces()
           await send(
-            .bookmarkedCafeResponse(cafes: bookmarkedCafes.map { BookmarkCafe(cafeData: $0, isBookmarked: true)})
+            .bookmarkedCafeResponse(cafes: bookmarkedCafes)
           )
         } catch: { error, send in
           debugPrint(error)
@@ -57,7 +53,7 @@ struct SavedList: ReducerProtocol {
 
       case .bookmarkButtonTapped(let cafe):
         if let index = state.cafes.firstIndex(of: cafe) {
-          state.cafes[index].isBookmarked.toggle()
+          state.cafes[index].isBookmarked?.toggle()
         }
         return .none
 
