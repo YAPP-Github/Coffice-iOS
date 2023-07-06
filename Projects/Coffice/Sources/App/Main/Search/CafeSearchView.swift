eb0156c23a4ef4b651e580a2f97bac093dfccf3b
Sehoon Jeong <wtd120120@gmail.com>
2023-07-06 11:00:45 +0900

    [UI] #93 TextFieldCursor 컬러 변경

--- Projects/Coffice/Sources/App/Main/Search/CafeSearchView.swift
+++ Projects/Coffice/Sources/App/Main/Search/CafeSearchView.swift
@@ -60,6 +60,7 @@ extension CafeSearchView {
             .padding(.trailing, 12)
           TextField("지하철, 카페 이름으로 검색", text: viewStore.binding(\.$searchText))
             .applyCofficeFontForTextField(font: .subtitle1Medium)
+            .tint(CofficeAsset.Colors.grayScale9.swiftUIColor)
             .foregroundColor(CofficeAsset.Colors.grayScale9.swiftUIColor)
             .onSubmit { viewStore.send(.submitText) }
             .textFieldStyle(.plain)
