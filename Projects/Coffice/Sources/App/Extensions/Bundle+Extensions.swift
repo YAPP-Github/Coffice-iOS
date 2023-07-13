4a7ac0daa67c3209b389671a10e8e3666abaa870
applebuddy <eagermin92@gmail.com>
2023-07-14 02:29:45 +0900

    [Chore] #153 ContactEmailView 구현 로직 중, 앱크래시 유발 가능성이 있는 코드 수정

--- Projects/Coffice/Sources/App/Extensions/Bundle+Extensions.swift
+++ Projects/Coffice/Sources/App/Extensions/Bundle+Extensions.swift
@@ -14,23 +14,26 @@ extension Bundle {
     from file: String,
     dateDecodingStategy: JSONDecoder.DateDecodingStrategy = .deferredToDate,
     keyDecodingStrategy: JSONDecoder.KeyDecodingStrategy = .useDefaultKeys
-  ) -> T {
+  ) -> T? {
     guard let url = CofficeResources.bundle.url(forResource: "DeviceModels", withExtension: "json")
     else {
-      fatalError("Error: Failed to locate \(file) in bundle.")
+      debugPrint("Error: Failed to locate \(file) in bundle.")
+      return nil
     }
     guard let data = try? Data(contentsOf: url)
     else {
-      fatalError("Error: Failed to load \(file) from bundle.")
+      debugPrint("Error: Failed to load \(file) from bundle.")
+      return nil
     }
-    
+
     let decoder = JSONDecoder()
     decoder.dateDecodingStrategy = dateDecodingStategy
     decoder.keyDecodingStrategy = keyDecodingStrategy
-    
+
     guard let loaded = try? decoder.decode(T.self, from: data)
     else {
-      fatalError("Error: Failed to decode \(file) from bundle.")
+      debugPrint("Error: Failed to decode \(file) from bundle.")
+      return nil
     }
     return loaded
   }
--- Projects/Coffice/Sources/App/Extensions/UIDevice+Extensions.swift
+++ Projects/Coffice/Sources/App/Extensions/UIDevice+Extensions.swift
@@ -13,18 +13,19 @@ extension UIDevice {
     let identifier: String
     let model: String
     static var all: [DeviceModel] {
-      Bundle.main.decode([DeviceModel].self, from: "DeviceModels.json")
+      Bundle.main.decode([DeviceModel].self, from: "DeviceModels.json") ?? []
     }
   }
   var modelName: String {
 #if targetEnvironment(simulator)
-    let identifier = ProcessInfo().environment["SIMULATOR_MODEL_IDENTIFIER"]!
+    let identifier = ProcessInfo().environment["SIMULATOR_MODEL_IDENTIFIER"] ?? ""
 #else
     var systemInfo = utsname()
     uname(&systemInfo)
     let machineMirror = Mirror(reflecting: systemInfo.machine)
     let identifier = machineMirror.children.reduce("") { identifier, element in
-      guard let value = element.value as? Int8, value != 0 else { return identifier }
+      guard let value = element.value as? Int8, value != 0
+      else { return identifier }
       return identifier + String(UnicodeScalar(UInt8(value)))
     }
 #endif
