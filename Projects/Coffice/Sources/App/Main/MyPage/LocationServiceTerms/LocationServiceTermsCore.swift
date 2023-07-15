3284f2dbb9f0b16f795d66cf318f9e50b8e6d5ed
Applebuddy <applebuddy@nate.com>
2023-07-16 03:00:30 +0900

    [UI] #175 위치기반 서비스 이용약관 화면 웹뷰 추가

--- Projects/Coffice/Sources/App/Main/MyPage/LocationServiceTerms/LocationServiceTermsCore.swift
+++ Projects/Coffice/Sources/App/Main/MyPage/LocationServiceTerms/LocationServiceTermsCore.swift
@@ -11,21 +11,37 @@ import ComposableArchitecture
 struct LocationServiceTerms: ReducerProtocol {
   struct State: Equatable {
     static let initialState: State = .init()
-
+    var webViewState: CommonWebReducer.State
+    let webUrlString = "https://traveling-jade-4ad.notion.site/f946b1a337704f108f11d3c6333569d8"
     let title = "위치서비스 약관"
+
+    init() {
+      webViewState = .init(urlString: webUrlString)
+    }
   }
 
   enum Action: Equatable {
     case onAppear
     case popView
+    case webAction(CommonWebReducer.Action)
   }
 
   @Dependency(\.apiClient) private var apiClient
 
   var body: some ReducerProtocolOf<LocationServiceTerms> {
+    Scope(
+      state: \.webViewState,
+      action: /Action.webAction
+    ) {
+      CommonWebReducer()
+    }
+
     Reduce { _, action in
       switch action {
       case .onAppear:
+        return .none
+
+      case .webAction:
         return .none
 
       case .popView:
