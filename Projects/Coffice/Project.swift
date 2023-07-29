70a640dac43363c54a9a580ce2d0fa02ee555e98
SooHyun Chun <neph3779@gmail.com>
2023-07-29 15:29:58 +0900

    Fix/#263 저장탭 이탈시 북마크 delete 안되던 것 수정 (#264)
    * [Fix] #263 저장탭 나갈때 onDisappear 호출 안하던 것 수정
    
    * [Chore] #263 현재 버전에 맞게 프로젝트 파일 수정

--- Projects/Coffice/Project.swift
+++ Projects/Coffice/Project.swift
@@ -10,8 +10,8 @@ let project = Project.app(
   platform: .iOS,
   iOSTargetVersion: iOSTargetVersion,
   infoPlist: [
-    "CFBundleShortVersionString": "1.0.0", // 앱의 출시 버전
-    "CFBundleVersion": "3", // 앱의 빌드 버전 (테스트 플라이트 배포시 빌드 버전 up 필요)
+    "CFBundleShortVersionString": "1.0.1", // 앱의 출시 버전
+    "CFBundleVersion": "0", // 앱의 빌드 버전 (테스트 플라이트 배포시 빌드 버전 up 필요)
     "CFBundleDisplayName": "coffice", // 사용자에게 보여질 앱의 이름
     "UILaunchStoryboardName": "LaunchScreen",
     "UIInterfaceOrientation": ["UIInterfaceOrientationPortrait"],
