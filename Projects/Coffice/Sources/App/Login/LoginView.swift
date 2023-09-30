543b095f04b406019dd260d79be184562461c2bc
Applebuddy <applebuddy@nate.com>
2023-09-30 21:23:10 +0900

    [Fix] #317 로그인 뷰 좌상단 X버튼 레이아웃 수정

--- Projects/Coffice/Sources/App/Login/LoginView.swift
+++ Projects/Coffice/Sources/App/Login/LoginView.swift
@@ -76,18 +76,20 @@ struct LoginView: View {
         }
         .padding(EdgeInsets(top: 163, leading: 36, bottom: 129, trailing: 36))
       }
-      .overlay(alignment: .topLeading) {
-        Group {
+      .navigationBarHidden(true)
+      .overlay(
+        alignment: .topLeading,
+        content: {
           Button {
             viewStore.send(.dismissButtonTapped)
           } label: {
             CofficeAsset.Asset.close40px.swiftUIImage
-              .frame(width: 40, height: 40)
+              .padding(.top, 4)
+              .padding(.leading, 8)
           }
+          .hiddenWithOpacity(isHidden: viewStore.isOnboarding)
         }
-        .padding(20)
-        .hiddenWithOpacity(isHidden: viewStore.isOnboarding)
-      }
+      )
       .popup(
         item: viewStore.binding(
           get: \.loginServiceTermsBottomSheetState,
