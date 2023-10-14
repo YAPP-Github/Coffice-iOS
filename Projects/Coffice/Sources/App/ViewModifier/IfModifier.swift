396886758de885223b1a2d8abdaf6cc120df70aa
hanjaeseung <31721888+hanjaesueng@users.noreply.github.com>
2023-10-14 23:13:30 +0900

    [Fix] #333 - CafeCardView 애니메이션 추가 이후 bottomFloatingButtonView 이 가운데 정도에 위치해있는 버그 디버깅 (#334)
    * [Fix] #333 - CafeCardView 애니메이션 추가 이후 bottomFloatingButtonView 이 가운데 정도에 위치해있는 버그 디버깅
    
    * [Fix, 파일 작성자 이름 변경]

---
+++ Projects/Coffice/Sources/App/ViewModifier/IfModifier.swift
@@ -0,0 +1,24 @@
+//
+//  IfModifier.swift
+//  coffice
+//
+//  Created by jaeseunghan on 2023/10/14.
+//  Copyright © 2023 kr.co.yapp. All rights reserved.
+//
+
+import SwiftUI
+
+extension View {
+  /// Applies the given transform if the given condition evaluates to `true`.
+  /// - Parameters:
+  ///   - condition: The condition to evaluate.
+  ///   - transform: The transform to apply to the source `View`.
+  /// - Returns: Either the original `View` or the modified `View` if the condition is `true`.
+  @ViewBuilder func `if`<Content: View>(_ condition: Bool, transform: (Self) -> Content) -> some View {
+    if condition {
+      transform(self)
+    } else {
+      self
+    }
+  }
+}
