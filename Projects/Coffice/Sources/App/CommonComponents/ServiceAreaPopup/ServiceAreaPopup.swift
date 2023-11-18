85643e8224f6bac8d8e6054d0dd3f7f0d528c8a6
Applebuddy <applebuddy@nate.com>
2023-11-18 18:33:30 +0900

    [Bug] #358 서비스 안내 문구 변경사양 반영

--- Projects/Coffice/Sources/App/CommonComponents/ServiceAreaPopup/ServiceAreaPopup.swift
+++ Projects/Coffice/Sources/App/CommonComponents/ServiceAreaPopup/ServiceAreaPopup.swift
@@ -9,7 +9,13 @@
 import ComposableArchitecture
 
 struct ServiceAreaPopup: Reducer {
-  struct State: Equatable { }
+  struct State: Equatable {
+    let title = "서비스 운영 지역"
+    let description = """
+                      현재 강남지역만 서비스 중이에요.
+                      카페 등록 요청은 문의하기를 이용해 주세요!
+                      """
+  }
 
   enum Action: Equatable {
     case confirmButtonTapped
