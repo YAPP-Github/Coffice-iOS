3c9ba692f4af38520f63483710feefcd54a67e6a
hanjaeseung <fx12wkd@naver.com>
2023-12-07 00:09:14 +0900

    [NONE, tuist 버전 고정, Dependencies의 url 변경]
    작업내용
     - tuist 버전 고정
     - dependencies에 변경된 url 적용

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -12,7 +12,7 @@ let dependencies = Dependencies(
   swiftPackageManager: [
     .remote(url: "https://github.com/johnpatrickmorgan/TCACoordinators.git", requirement: .upToNextMajor(from: "0.3.0")),
     .remote(url: "https://github.com/pointfreeco/swift-composable-architecture", requirement: .exact("1.0.0")),
-    .remote(url: "https://github.com/jaemyeong/NMapsMap-SPM.git", requirement: .upToNextMajor(from: "3.16.2")),
+    .remote(url: "https://github.com/jaemyeong/NMapsMap.git", requirement: .upToNextMajor(from: "3.17.0")),
     .remote(url: "https://github.com/firebase/firebase-ios-sdk.git", requirement: .upToNextMajor(from: "10.10.0")),
     .remote(url: "https://github.com/kakao/kakao-ios-sdk", requirement: .branch("master")),
     .remote(url: "https://github.com/onevcat/Kingfisher.git", requirement: .upToNextMajor(from: "7.0.0")),
